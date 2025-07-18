da08154fab37e4bad95e05ceeff0b277a1a963ca
sean.go <sean.go@kakaocorp.com>
2025-07-18 17:54:38 +0900

    1

--- components/gnb.tsx
+++ components/gnb.tsx
@@ -25,13 +25,6 @@ export function GNB() {
               </Link>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <Link href="/quick-order" legacyBehavior passHref>
-                <NavigationMenuLink className="px-4 py-2 bg-gradient-to-r from-orange-500 to-red-500 hover:from-orange-600 hover:to-red-600 text-white rounded-lg transition-all duration-200 font-medium border border-orange-400 hover:border-orange-500">
-                  🚀 빠른 주문
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
               <Link href="/shops" legacyBehavior passHref>
                 <NavigationMenuLink className="px-4 py-2 text-white hover:text-blue-100 hover:bg-white/10 rounded-lg transition-all duration-200 font-medium">
                   매장목록
@@ -41,7 +34,21 @@ export function GNB() {
             <NavigationMenuItem>
               <Link href="/register-shop" legacyBehavior passHref>
                 <NavigationMenuLink className="px-4 py-2 bg-white/20 hover:bg-white/30 text-white rounded-lg transition-all duration-200 font-medium border border-white/20 hover:border-white/30">
-                  매장 등록
+                  매장등록
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/register-menu" legacyBehavior passHref>
+                <NavigationMenuLink className="px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded-lg transition-all duration-200 font-medium border border-green-400 hover:border-green-500">
+                  📝 메뉴판등록
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href="/quick-order" legacyBehavior passHref>
+                <NavigationMenuLink className="px-4 py-2 bg-gradient-to-r from-orange-500 to-red-500 hover:from-orange-600 hover:to-red-600 text-white rounded-lg transition-all duration-200 font-medium border border-orange-400 hover:border-orange-500">
+                  🚀 빠른주문
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
